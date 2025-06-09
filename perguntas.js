8fb091a2d6d7fba43b1a7071a365fe19ba9290f7
samur101334 <samuel.oliveira.carvalho@escola.pr.gov.br>
2025-06-09 10:57:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+  'Historia'
+  'Como a revolução francesa influenciou o mundo moderno?'
+  'la marcou o fim da monarquia absolutista na frança e espalhou ideias como liberdade, igualdade e fraternidade.Inspirou outros movimentos revolucionarios e ajudou a consolidar a noção de cidadania e decretos individuais'
+)
+criaCartao(
+  'Historia'
+  'Quais foram as principais causas da segunda guerra mundial?'
+  'O Tratado de Versalhes, que impôs pesadas sanções à Alemanha após a Primeira Guerra Mundial,  a ascensão do nazismo e do fascismo na Itália e a política de apaziguamento seguida pelas potências ocidentais, que permitiram a expansão territorial da Alemanha.'
+)
+criaCartao(
+  'Historia'
+  'O que foi o Holocausto?'
+  'O Holocausto foi o assassinato sistemático de seis milhões de judeus pelos nazistas na Segunda Guerra Mundial, motivado por ódio racial. Milhões de outros também foram mortos. Aconteceu principalmente em campos de extermínio como Auschwitz. É um lembrete sombrio da importância de combater o preconceito.'
+)
+criaCartao(
+ 'Historia'
+ 'Qual foi o principal motivo da Revolução Francesa?'
+ 'A Revolução Francesa foi impulsionada principalmente pela insatisfação social e econômica do povo com a monarquia absolutista e a desigualdade entre as classes.'
+)
+criaCartao(
+ 'Historia'
+ 'Quem foi Mahatma Gandhi e pelo que ele é conhecido?'
+ 'Mahatma Gandhi foi um líder político e espiritual indiano que é famoso por sua filosofia de resistência não violenta (satyagraha) na luta pela independência da Índia do domínio britânico.'
+)
+criaCartao(
+ 'Historia'
+ 'O que marcou o início da Idade Média na Europa?'
+ 'O início da Idade Média, por volta do século V, foi marcado pela queda do Império Romano do Ocidente e o subsequente período de fragmentação política e social na Europa.'
+)
+criaCartao(
+ 'Historia'
+ 'Qual a importância da invenção da prensa móvel por Gutenberg?'
+ 'A invenção da prensa móvel por Johannes Gutenberg no século XV foi revolucionária, pois permitiu a produção em massa de livros, o que democratizou o acesso ao conhecimento e impulsionou a Reforma Protestante e o Renascimento.'
+)
